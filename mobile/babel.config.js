3f02b094e92b18373c1f72363f3941d31ab09d6d
vietvohoangnashtech <viet.vohoang@nashtechglobal.com>
2026-03-30 13:44:09 +0700

    Add env

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,5 +2,11 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     ['@babel/plugin-proposal-decorators', {legacy: true}],
+    ['module:react-native-dotenv', {
+      moduleName: '@env',
+      path: '.env',
+      safe: false,
+      allowUndefined: false,
+    }],
   ],
 };
--- mobile/src/services/api/client.ts
+++ mobile/src/services/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import {API_BASE_URL} from '@env';
 
 const apiClient = axios.create({
-  baseURL: 'http://10.0.2.2:3000',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
